10c97586a173b66dfbebfcb6daaf96507471b98f
takukakku <takumi.kakutani@rakuten-sec.co.jp>
2020-09-15 19:01:17 +0900

    kkkk

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php echo "hello" ?>
-<?php echo "see you" ?>
-<?php echo "see you after" ?>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <title>tech boost</title>
+
+  <style>
+  h1 {
+    color: red;
+  }
+  </style>
+
+</head>
+  <body>
+    <h1>Hello world!!!</h1>
+    <p>CSSの入門！</p>
+  </body>
+</html>
